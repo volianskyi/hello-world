a9a0a842df50974ca0c8192979cc79b4dff7f923
Ihor Volianskyi <volyanskiy.i@gmail.com>
2022-11-22 21:35:56 +0200

    add word

--- src/main/java/com/bazaarvoice/Application.java
+++ src/main/java/com/bazaarvoice/Application.java
@@ -2,6 +2,6 @@ package com.bazaarvoice;
 
 public class Application {
     public static void main(String[] args) {
-        System.out.println("Hello");
+        System.out.println("Hello world");
     }
 }
