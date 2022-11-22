8d2bb0b962fee6d853ed912677578eaad3b2caae
Ihor Volianskyi <volyanskiy.i@gmail.com>
2022-11-22 21:51:44 +0200

    add logger (#1)

--- src/main/java/com/bazaarvoice/Application.java
+++ src/main/java/com/bazaarvoice/Application.java
@@ -1,7 +1,10 @@
 package com.bazaarvoice;
 
+import lombok.extern.slf4j.Slf4j;
+
+@Slf4j
 public class Application {
     public static void main(String[] args) {
-        System.out.println("Hello world");
+        log.info("Hello world");
     }
 }
